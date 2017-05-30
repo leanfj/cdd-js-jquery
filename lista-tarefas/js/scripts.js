eb6a988153b0be56ce70f5ab3aabb96c49f33ac7
leanfj <leanfj@hotmail.com>
2017-05-30 01:15:35 -0300

    Criado exercício de lista de tarefas

---
+++ lista-tarefas/js/scripts.js
@@ -0,0 +1,16 @@
+$('#tarefa').on("keydown.primeiro", function (event) {
+  console.log(event.which, String.fromCharCode(event.which));
+});
+
+// String.fromCharCode() converte o código para o respectivo caractere
+// Propriedade which retorna o codigo Unicode
+
+$('#tarefa').on("keydown.segundo", function (event) {
+  if (event.which === 13) {
+    console.log("enter");
+
+  }
+});
+
+// Namespace utilizado para identificar o evento e desassocia-lo
+$('#tarefa').off("keydown.primeiro");
