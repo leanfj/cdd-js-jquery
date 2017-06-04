0a8cb7234d28d93753d20a7b3f77c88b1c9f8411
leanfj <leanfj@hotmail.com>
2017-06-04 00:48:24 -0300

    Alteração de arquivo javascript para selecionar elemento de texto

--- lista-tarefas/js/todo.js
+++ lista-tarefas/js/todo.js
@@ -1,9 +1,25 @@
-
+// Associa eventos assim que o doc é carregado
 $(document).ready(
+
   $(function(){
     function onTarefaDeleteClick () {
-      console.log("Cliquei na lixeira!!");
+      // console.log("Cliquei na lixeira!!");
+      // Metodo Parent utilizado para selecionar o elemento pai
+      console.log($(this).parent('.tarefa-item').text().trim());
+
+      // Metodo hide utilizado para esconder o elemento, parametro slow para aplicar efeito. Metodo remove elemina o elemento do DOM
+      // Primeiro this se refere ao elemento tarefa-delete associado ao evento onde a função vai ser chamada
+      // O segundo thi se refere ao elemento tarefa-item
+      $(this).parent('.tarefa-item').hide('slow', function () {
+        $(this).remove();
+      });
     }
     $(".tarefa-delete").click(onTarefaDeleteClick);
+
+    function onTarefaItemClick() {
+      console.log("Clique para editar");
+    }
+
+    $('.tarefa-item').click(onTarefaItemClick);
   })
 );
